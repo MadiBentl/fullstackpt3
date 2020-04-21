ae443857412125cd6275f81f98cdf69304aaea5d
Madison Bentley <mb.madisonbentley@gmail.com>
2020-04-20 20:49:43 -0700

    pushed  build

---
+++ build/precache-manifest.6ca96f83481f65578f3b06e73606a6e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4426ad0b714529432da42dbc5acf7cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7102960b299aaabfcd31",
+    "url": "/static/js/2.55d36d8d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.55d36d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507a4d4911712770c08d",
+    "url": "/static/js/main.c3db908e.chunk.js"
+  },
+  {
+    "revision": "c1ca01838fd0fef55ded",
+    "url": "/static/js/runtime-main.2a146b8e.js"
+  }
+]);
---
+++ build/precache-manifest.f010c92c9fbdf97bccb1e7f993bf6b98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d0343fc136f85df72e8a589ba6c1b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7102960b299aaabfcd31",
+    "url": "/static/js/2.55d36d8d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.55d36d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3afdcdab419d08a406b2",
+    "url": "/static/js/main.06906c4b.chunk.js"
+  },
+  {
+    "revision": "c1ca01838fd0fef55ded",
+    "url": "/static/js/runtime-main.2a146b8e.js"
+  }
+]);
