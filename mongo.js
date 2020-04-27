6643e41944e5712668d27943314d029b3e586038
Madison Bentley <mb.madisonbentley@gmail.com>
2020-04-26 19:30:01 -0700

    updated notes sample app to most recent version

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length <3 ){
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://madibentl:${password}@cluster0-cru25.mongodb.net/note-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology: true})
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is Hard',
+  date: new Date(),
+  important: true,
+})
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
+
+//note.save().then(response => {
+  //console.log('note saved!')
+  //mongoose.connection.close()
+//})
